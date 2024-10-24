0885d9d3a59090e64d681d7ab305933222e42474
pyquinte <pyquinte@cibertec.com>
2024-10-24 00:15:07 -0500

    Primer commit

---
+++ src/main/java/com/cibertec/controller/ProyectoController.java
@@ -0,0 +1,34 @@
+package com.cibertec.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProyectoController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+	@GetMapping("/home")
+	public String home(){
+		return "home";
+	}
+	@GetMapping("/contact")
+	public String contact(){
+		return "contact";
+	}
+	@GetMapping("/product")
+	public String product(){
+		return "product";
+	}
+
+
+
+}
